27ce9f0789a83a3817bccb1af4e97ddc6993d8d4
walace67 <walace1967@gmail.com>
2021-10-27 13:42:21 -0400

    final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,21 @@
 import { Request, Response } from 'express';
 
-export const search = (req: Request, res: Response) => {
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObject';
 
-    // res.render('pages/search');
+export const search = (req: Request, res: Response) => {
+    let query: string = req.query.q as string;
+
+    let list = Pet.getFromName(query);
+
+    if(!query) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    });
 }
